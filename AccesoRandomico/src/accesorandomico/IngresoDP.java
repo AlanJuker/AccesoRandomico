1799ef1c9467c6f58165dda7ceb29a0b17230e14
toka404 <58258902+toka404@users.noreply.github.com>
2021-05-05 20:02:15 -0500

    Final ingresoDP

--- AccesoRandomico/src/accesorandomico/IngresoDP.java
+++ AccesoRandomico/src/accesorandomico/IngresoDP.java
@@ -5,6 +5,10 @@
  */
 package accesorandomico;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
 /**
  *
  * @author bryan
@@ -20,38 +24,56 @@ public class IngresoDP {
         this.nombre = nombre;
         this.edad = edad;
     }
-    public IngresoDP()
-    {
+
+    public IngresoDP() {
     }
-    
-    public void definirClave(int clave)
-    {
+
+    public void definirClave(int clave) {
         this.clave = clave;
     }
-    
-    public int obtenerClave()
-    {
+
+    public int obtenerClave() {
         return clave;
     }
-    public void definriNombre(String nombre){
+
+    public void definirNombre(String nombre) {
         this.nombre = nombre;
     }
-    public String obtenerNombre()
-    {
+
+    public String obtenerNombre() {
         return nombre;
     }
-    public void defirnirEdad(int edad)
-    {
+
+    public void defirnirEdad(int edad) {
         this.edad = edad;
     }
-    public int obtenerEdad()
-    {
+
+    public int obtenerEdad() {
         return edad;
     }
-    
-    public void grabarDatos(){
-        
+
+    public void grabarDatos() {
+        String raiz = System.getProperty("user.dir");
+        try {
+            File arch = new File(raiz + "\\archivo1.dat");
+            RandomAccessFile archivo = new RandomAccessFile(arch, "rw");
+
+            if (archivo.length() != 0) {
+                archivo.seek(archivo.length());
+                archivo.writeInt(clave);
+                archivo.writeChars(nombre);
+                archivo.writeInt(edad);
+                archivo.close();
+            } else {
+                archivo.writeInt(clave);
+                archivo.writeChars(nombre);
+                archivo.writeInt(edad);
+                archivo.close();
+            }
+        } catch (IOException e) {
+            System.out.println("errore en el ingreso");
+        }
+
     }
-    
 
 }
