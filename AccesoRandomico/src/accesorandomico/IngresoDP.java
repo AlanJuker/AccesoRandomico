d367be7e084ea08c15e30fe364fee013dbbf2391
toka404 <58258902+toka404@users.noreply.github.com>
2021-05-05 19:35:58 -0500

    Metodos IngresoDP
    Falta el codigo del emtodo de guardado

--- AccesoRandomico/src/accesorandomico/IngresoDP.java
+++ AccesoRandomico/src/accesorandomico/IngresoDP.java
@@ -10,7 +10,48 @@ package accesorandomico;
  * @author bryan
  */
 public class IngresoDP {
+
+    int clave;
+    String nombre;
+    int edad;
+
+    public IngresoDP(int clave, String nombre, int edad) {
+        this.clave = clave;
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+    public IngresoDP()
+    {
+    }
     
-    // el jared estuvo aqui
-    // Comentario de prueba AA
+    public void definirClave(int clave)
+    {
+        this.clave = clave;
+    }
+    
+    public int obtenerClave()
+    {
+        return clave;
+    }
+    public void definriNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public String obtenerNombre()
+    {
+        return nombre;
+    }
+    public void defirnirEdad(int edad)
+    {
+        this.edad = edad;
+    }
+    public int obtenerEdad()
+    {
+        return edad;
+    }
+    
+    public void grabarDatos(){
+        
+    }
+    
+
 }
